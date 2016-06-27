3cde7dd5707c02bcbb4b1e4668aa54839dd451b5
carlosthe19916 <carlosthe19916@gmail.com>
2016-06-26 23:23:18 -0500

    RE-02 add simple components

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component {
+    selector: 'about',
+    pipes: [],
+    providers: [],
+    directives: [],
+    styleUrls: ['./about.component.css'],
+    templateUrl: './about.component.html'
+}
+
+export class AboutComponent {
+    constructor(http: Http) {
+
+    }
+
+    ngOnInit() {
+        
+    }
+}
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component {
+    selector: 'home',
+    pipes: [],
+    providers: [],
+    directives: [],
+    styleUrls: ['./home.component.css'],
+    templateUrl: './home.component.html'
+}
+
+export class AboutComponent {
+    constructor(http: Http) {
+
+    }
+
+    ngOnInit() {
+        
+    }
+}
