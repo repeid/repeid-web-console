835a6c2293a0bdfaa2d21fdf50859aa6d4af13d2
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2016-07-11 08:44:32 -0500

    styles and fonts out side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MyComponentComponent } from './my-component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
+  styleUrls: ['app.component.css', '../styles/main.css'],
+  encapsulation: ViewEncapsulation.None,
   directives: [MyComponentComponent]
 })
 export class AppComponent {
--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {AlertComponent, DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   moduleId: module.id,
   selector: 'app-my-component',
   templateUrl: 'my-component.component.html',
-  styleUrls: ['my-component.component.css', './styles/main.css'],
-  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['my-component.component.css'],
   directives: [AlertComponent, DATEPICKER_DIRECTIVES]
 })
 export class MyComponentComponent implements OnInit {
