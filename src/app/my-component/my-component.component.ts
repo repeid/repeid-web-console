473a4fc306861b0ca38f2f20279be243933412ac
Carlos Feria <carlosthe19916@gmail.com>
2016-07-11 00:05:41 -0500

    sass

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {AlertComponent, DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   moduleId: module.id,
   selector: 'app-my-component',
   templateUrl: 'my-component.component.html',
-  styleUrls: ['my-component.component.css'],
+  styleUrls: ['my-component.component.css', './styles/main.css'],
+  encapsulation: ViewEncapsulation.None,
   directives: [AlertComponent, DATEPICKER_DIRECTIVES]
 })
 export class MyComponentComponent implements OnInit {
